1ef460cab00191a3cbeb7d75fe4dc9465bca2ebe
naufal03 <naufalarkan134@gmail.com>
2021-09-22 13:14:03 +0700

    web php

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// ini route list componen
+// ini route list componen web.php
 Route::get('/', function () {
     return view('welcome');
 });
